24889c89a3fc1d959a384001099adf42263e5dfc
Steven Santiago-Perez <santiagoperezsteven@gmail.com>
2025-05-29 19:18:33 -0400

    validateUser working

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -29,8 +29,15 @@ async function validateUserId (req, res, next) {
 }
 
 function validateUser (req, res, next) {
-  console.log('validateUser middleware')
-  next()
+  const { name } = req.body;
+  if ( !name || !name.trim() ) {
+    res.status(400).json({
+      message: "missing required name field",
+    })
+  } else {
+    req.name = name.trim();
+    next();
+  }
 }
 
 function validatePost (req, res, next) {
