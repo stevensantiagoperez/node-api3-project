5450bebd4a879baa8098c3cb4e81765f60327a3d
Steven Santiago-Perez <santiagoperezsteven@gmail.com>
2025-05-29 18:30:24 -0400

    logger done

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,30 @@
-function logger(req, res, next) {
-  // DO YOUR MAGIC
+function logger (req, res, next) {
+  const timestamp = new Date().toLocaleString();
+  const method = req.method;
+  const url = req.originalUrl;
+  console.log(`[${timestamp}] ${method} to ${url} `);
+  next();
 }
 
-function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+function validateUserId (req, res, next) {
+  console.log('validateUserId middleware')
+  next()
 }
 
-function validateUser(req, res, next) {
-  // DO YOUR MAGIC
+function validateUser (req, res, next) {
+  console.log('validateUser middleware')
+  next()
 }
 
-function validatePost(req, res, next) {
-  // DO YOUR MAGIC
+function validatePost (req, res, next) {
+  console.log('validatePost middleware')
+  next()
 }
 
 // do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost,
+}
--- api/server.js
+++ api/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { logger } = require('./middleware/middleware')
 
 const server = express();
 
@@ -8,6 +9,8 @@ server.use(express.json());
 // remember express by default cannot parse JSON in request bodies
 
 // global middlewares and the user's router need to be connected here
+
+server.use(logger)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
