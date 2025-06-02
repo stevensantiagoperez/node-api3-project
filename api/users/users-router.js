470e07884e47947d0e17d5b825b0d23526739b1e
Steven Santiago-Perez <santiagoperezsteven@gmail.com>
2025-06-02 19:20:27 -0400

    wip

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -57,7 +57,7 @@ router.put('/:id', validateUserId, validateUser, (req, res, next) => {
   .catch(next)
 });
 
-router.delete('/:id', validateUserId, async (req, res) => {
+router.delete('/:id', validateUserId, async (req, res, next) => {
   // RETURN THE FRESHLY DELETED USER OBJECT
   // this needs a middleware to verify user id
   try{
@@ -68,18 +68,34 @@ router.delete('/:id', validateUserId, async (req, res) => {
   }
 });
 
-router.get('/:id/posts', validateUserId, (req, res) => {
+router.get('/:id/posts', validateUserId, async (req, res, next) => {
   // RETURN THE ARRAY OF USER POSTS
   // this needs a middleware to verify user id
-  console.log(req.user)
+  try{
+   const result = await User.getUserPosts(req.params.id)
+   res.json(result)
+  } catch (err) {
+    next(err)
+  }
 });
 
-router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
+router.post(
+  '/:id/posts', 
+  validateUserId, 
+  validatePost, 
+  async (req, res, next) => {
   // RETURN THE NEWLY CREATED USER POST
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
-  console.log(req.user)
-  console.log(req.text)
+  try{
+    const result = await Post.insert({
+      user_id: req.params.id,
+      text: req.text,
+    })
+    res.status(201).json(result)
+  } catch (err){
+    next(err)
+  }
 });
 
 router.use((err,req, res, next) =>{ // eslint-disable-line 
